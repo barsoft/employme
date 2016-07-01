60ac6f9028cc1dfedd931383fe34901771dbf6a1
Maksym Prysiazhnyi <maks9416@gmail.com>
2016-07-02 00:30:04 +0200

    2.7.2016

--- src/main/java/com/barsoft/employme/DataConnect.java
+++ src/main/java/com/barsoft/employme/DataConnect.java
@@ -5,26 +5,30 @@ import java.sql.DriverManager;
 
 public class DataConnect {
 
-	public static Connection getConnection() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			// Connection con = DriverManager.getConnection(
-			// "jdbc:mysql://127.12.5.2:3306/employmeex?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull",
-			// "adminpgfczzm", "SDTCsF8N54Yf");
-			Connection con = DriverManager.getConnection(
-					"jdbc:mysql://127.0.0.1/employme?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull",
-					"root", "");
-			return con;
-		} catch (Exception ex) {
-			System.out.println("Database.getConnection() Error -->" + ex.getMessage());
-			return null;
-		}
-	}
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con;
+            try {
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://127.3.18.2:3306/employme?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull",
+                        "admingGMrHPx", "3ll8HlJYyQM4");
+            } catch (Exception ex) {
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://127.0.0.1/employme?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull",
+                        "root", "");
+            }
+            return con;
+        } catch (Exception ex) {
+            System.out.println("Database.getConnection() Error -->" + ex.getMessage());
+            return null;
+        }
+    }
 
-	public static void close(Connection con) {
-		try {
-			con.close();
-		} catch (Exception ex) {
-		}
-	}
+    public static void close(Connection con) {
+        try {
+            con.close();
+        } catch (Exception ex) {
+        }
+    }
 }
